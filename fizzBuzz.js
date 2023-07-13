216e415da933121aed9033e1624c7448739903f4
Scott Nelson <scottninlv@yahoo.com>
2023-07-13 08:24:05 -0700

    FizzBuzz chall Finish

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// Write your solution below this line:
+
+for (i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log('Fizz')
+    } else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
